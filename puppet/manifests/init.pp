588c3ee2863e2a88763d4273096ac359ae38880e
victorhdamian <victorhdamian@gmail.com>
2014-11-06 17:24:09 -0500

    changed init.pp

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -1,11 +1,4 @@
-    node default {
-      class { 'nginx': }
-    }
-
-    node default {
-      class { 'nginx': }
-      nginx::resource::vhost { 'www.puppetlabs.com':
-        ensure   => present,
-        www_root => '/var/www/www.puppetlabs.com',
-      }
-    }
+::puppet
+package {"nginx":
+    ensure => present,
+}
