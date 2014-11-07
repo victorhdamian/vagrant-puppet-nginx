72e5632ac81116aa01f0f57c2718a30cfc7c92be
victorhdamian <victorhdamian@gmail.com>
2014-11-07 13:43:08 -0500

    changed nginx module and dependencies

--- puppet/modules/nginx/manifests/service.pp
+++ puppet/modules/nginx/manifests/service.pp
@@ -14,33 +14,24 @@
 #
 # This class file is not called directly
 class nginx::service(
-  $configtest_enable = $nginx::configtest_enable,
-  $service_restart   = $nginx::service_restart,
-  $service_ensure    = $nginx::service_ensure,
+  $configtest_enable   = $nginx::params::nx_configtest_enable,
+  $service_restart     = $nginx::params::nx_service_restart
 ) {
-
-  $service_enable = $service_ensure ? {
-    running => true,
-    absent => false,
-    stopped => false,
-    'undef' => undef,
-    default => true,
+  exec { 'rebuild-nginx-vhosts':
+    command     => "/bin/cat ${nginx::params::nx_temp_dir}/nginx.d/* > ${nginx::params::nx_conf_dir}/conf.d/vhost_autogen.conf",
+    refreshonly => true,
+    unless	=> "/usr/bin/test ! -f ${nginx::params::nx_temp_dir}/nginx.d/*",
+    subscribe   => File["${nginx::params::nx_temp_dir}/nginx.d"],
   }
-
-  if $service_ensure == 'undef' {
-    $service_ensure_real = undef
-  } else {
-    $service_ensure_real = $service_ensure
-  }
-
-  service { 'nginx':
-    ensure     => $service_ensure_real,
-    enable     => $service_enable,
+  service { "nginx":
+    ensure     => running,
+    enable     => true,
     hasstatus  => true,
     hasrestart => true,
+    subscribe  => Exec['rebuild-nginx-vhosts'],
   }
   if $configtest_enable == true {
-    Service['nginx'] {
+    Service["nginx"] {
       restart => $service_restart,
     }
   }
