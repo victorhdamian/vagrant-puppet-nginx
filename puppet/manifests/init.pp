14b587a753a06fba6f1d422ef3d5d2d0323bfda0
victorhdamian <victorhdamian@gmail.com>
2014-11-07 17:07:20 -0500

    edited init.pp

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -1,7 +1,7 @@
     case $operatingsystem {
       'Ubuntu': { 
 	exec { "apt-get install nginx":
-    	command => "sudo apt-get install nginx",
+    	command => "apt-get install nginx",
     	path    => "/usr/local/bin/:/bin/",
 	}	
       }
