361e6ab358e3a81ac66d57fe0ce872a1b0e0485f
victorhdamian <victorhdamian@gmail.com>
2014-11-07 08:37:43 -0500

    changed init.pp

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -1,5 +1,19 @@
-class { 'nginx': }
+class { 'nginx': 
+
+	file { "/var/www/":
+	    ensure => "directory",	    
+	}
+
+	file { "/var/www/www.puppetlabs.com":
+	    replace => "no", # this is the important property
+	    ensure  => "present",
+	    mode    => 644,
+            require => File['/var/www/'],
+	}
+
+}
 
 nginx::resource::vhost { 'www.puppetlabs.com':
   www_root => '/var/www/www.puppetlabs.com',
+  
 }
