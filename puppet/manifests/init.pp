2cf487dc5f3890c8490a31dbf5866afd56517347
victorhdamian <victorhdamian@gmail.com>
2014-11-07 17:36:21 -0500

    modified init.pp

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -1,10 +1,1 @@
-    case $operatingsystem {
-      'Ubuntu': { 
-	exec { "apt-get install nginx":
-    	command => "apt-get install nginx",
-    	path    => "/usr/local/bin/:/bin/",
-	}	
-      }
-      'CentOS': {   }
-      default:  {  }
-    }
+class { "nginx": }
