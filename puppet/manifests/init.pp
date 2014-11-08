9791b1db4b5de64e2ebe00fdfd5167a65703771a
victorhdamian <victorhdamian@gmail.com>
2014-11-08 16:16:43 -0500

    added nginx::resource::location to init.pp

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -1,6 +1,11 @@
 class { "nginx": }
 #todo
 #enable default site
-
+  nginx::resource::location { 'exersice':
+    ensure   => present,
+    www_root => '/var/www/nginx-default',
+    location => '/nginx-default',
+    vhost    => 'exersice',
+  }
 #edit index.html
 #<meta http-equiv="refresh" content="0; url=https://github.com/puppetlabs/exercise-webpage" />
