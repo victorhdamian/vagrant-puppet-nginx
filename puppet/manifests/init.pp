1515e2033bd2b3483f0b3367e7a0da228b75d02b
victorhdamian <victorhdamian@gmail.com>
2014-11-07 19:31:11 -0500

    modified init.pp

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -3,7 +3,8 @@ class { "nginx":
 	file { "/etc/nginx/nginx.conf":
 	   replace => "yes", # this is the important property
 	   ensure  => "present",
-	   content => "user www-data;
+	   content => "
+user www-data;
 worker_processes 1;
 
 error_log  /var/log/nginx/error.log;
@@ -31,16 +32,56 @@ http {
   gzip         on;
   gzip_disable "MSIE [1-6]\.(?!.*SV1)";
   
-
+ 
   include /etc/nginx/conf.d/*.conf;
   include /etc/nginx/sites-enabled/*;
-}",
+}
+	   
+	   ",
 	}
 
+	file { "/etc/nginx/sites-available/default":
+	   replace => "yes", # this is the important property
+	   ensure  => "present",
+	   content => "
+server {
+	listen   8000 default;
+	server_name  localhost;
+
+	access_log  /var/log/nginx/localhost.access.log;
+
+	location / {
+		root   /var/www/nginx-default;
+		index  index.html index.htm;
+	}
+
+	location /doc {
+		root   /usr/share;
+		autoindex on;
+		allow 127.0.0.1;
+		deny all;
+	}
+
+	location /images {
+		root   /usr/share;
+		autoindex on;
+	}
+}	   
+	   ",
+	}
+
+	file { "/var/www/nginx-default/index.htm":
+	   replace => "yes", # this is the important property
+	   ensure  => "present",
+	   content => "
+<meta http-equiv="refresh" content="0; url=https://github.com/puppetlabs/exercise-webpage" />	   
+	   ",
+	}
+	
 	file { '/etc/nginx/sites-enable/default':
 	   ensure => 'link',
 	   target => '/etc/nginx/sites-available/default',
-	   require => File['/etc/ssh/sshd_config'],
+	   require => File['/etc/nginx/nginx.conf'],
 	}
 	
 	service { "nginx":
