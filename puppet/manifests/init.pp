672f3a72e5f805fec8f74e54980bf6556b8faff4
victorhdamian <victorhdamian@gmail.com>
2014-11-08 07:30:16 -0500

    added  nginx::resource::vhost to init.pp

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -1,1 +1,8 @@
 class { "nginx": }
+nginx::resource::vhost { 'test2.local':
+    ensure   => present,
+    www_root => '/var/www/nginx-default',
+    ssl      => 'true',
+    ssl_cert => '/tmp/server.crt',
+    ssl_key  => '/tmp/server.pem',
+}
