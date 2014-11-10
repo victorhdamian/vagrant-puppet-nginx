94fa9e0f747f89279494acad42930d574d99aea9
victorhdamian <victorhdamian@gmail.com>
2014-11-10 06:48:00 -0500

    Tested Ubuntu

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -1,6 +1,19 @@
 class { "nginx": } ->
-file { '/var/www/nginx-default/index.html':
-  replace => "yes",
-  ensure  => file,
-  content => '<meta http-equiv="refresh" content="0; url=https://github.com/puppetlabs/exercise-webpage" />',
+
+#TODO
+#support for 
+#  'Fedora': {} 
+#  'CentOS': {}
+#  'Debian': {} 
+#  'Redhat': {}
+#   default: {} 
+
+case $operatingsystem {
+  'Ubuntu': {
+    file { '/var/www/nginx-default/index.html':
+      replace => "yes",
+      ensure  => file,
+      content => '<meta http-equiv="refresh" content="0; url=https://github.com/puppetlabs/exercise-webpage" />',
+    }     
+  } 
 }
